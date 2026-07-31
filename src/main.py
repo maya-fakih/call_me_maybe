798210e455aa2511f6adf0c63c937c8f1b6fda84
Maya Fakih <mfakih@c2r7s2.42beirut.com>
2026-07-31 13:03:18 +0300

    parsing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 import sys
-import argparser
+import argparse
 
 def input_validation():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--functions_definition",
+        default= "data/input/functions_definition.json"
+    )
+    parser.add_argument(
+        "--input",
+        default = "data/input/function_calling_tests.json"
+    )
+    parser.add_argument(
+        "--output",
+        default = "data/output/function_calls.json"
+    )
+    return parser.parse_args()
+
 
 
 def main():
-    input_validation()
+    args = input_validation()
+    for name, value in vars(args).items():
+        print (name, value)
 
-    pass
-
-if __name__== 'main':
+if __name__== '__main__':
     main()
